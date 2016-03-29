ce1d54e3b51e7dbb9d8d23b55a9ee18fd4a0f641
Tsvetomir <tz_popov@abv.bg>
2016-03-29 21:38:17 +0300

    Auth
    Tinkered a little bit with the authentication. Not done by a longshot

--- laravel/database/migrations/2016_03_29_173140_create_user_table.php
+++ laravel/database/migrations/2016_03_29_173140_create_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
