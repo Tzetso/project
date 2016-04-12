358285236957e3ec21965f3e44fb11811993bb1d
Tsvetomir <tz_popov@abv.bg>
2016-04-12 17:10:05 +0300

    Last fix I hope

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,45 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome 
-                	@if($user) 
-                		{{ $user->username}}
-                	@else                 
-                    	Guest
+<div class="table-container">
+    <div class="table">
+                <div class="table-heading">Welcome
+                    @if($user)
+                    {{ $user->username}}
+                    @else
+                    Guest
                     @endif
                 </div>
 
-                <div class="panel-body">
-                    
-                    <table class="table">		                
-	                	<thead>		                	
-	                		<tr>
-		                		<th>#</th>
-		                		<th><a href="{{route('welcome', ['sortBy' => 'username', 'order' => '$uOrder'])}}">Username</a></th>
-		                		<th><a href="{{route('welcome', ['sortBy' => 'highscore', 'order' => '$sOrder'])}}">Score</a></th>
-		                		<th><a href="{{route('welcome', ['sortBy' => 'currency', 'order' => '$mOrder'])}}">Money</a></th>
-	                		</tr>		                		
-	                	</thead>
-	                	
-	                	<tbody>
-	                    	@foreach($users as $user)
-		                		<tr>
-		                			<td>{{ $count++ }}</td>
-									<td>{{$user->username}}</td>
-		                    	 	<td>{{$user->highscore}}</td>	
-		                    	 	<td>{{$user->currency}}</td>                       	
-		                  	  	</tr>
-	               			@endforeach
-	               			{!! $users->render() !!}
-                    	</tbody>     		                	        
-		           	</table>
+                <div class="table-body">
+
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th><a href="{{route('welcome', ['sortBy' => 'username', 'order' => '$order'])}}">Username</a></th>
+                            <th><a href="{{route('welcome', ['sortBy' => 'highscore', 'order' => '$order'])}}">Score</a></th>
+                            <th><a href="{{route('welcome', ['sortBy' => 'currency', 'order' => '$order'])}}">Money</a></th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                            <td>{{ $count++ }}</td>
+                            <td>{{$user->username}}</td>
+                            <td>{{$user->highscore}}</td>
+                            <td>{{$user->currency}}</td>
+                        </tr>
+                        @endforeach
+                        
+                        </tbody>
+                        
+                    </table>
+                    <div>{!! $users->render() !!}</div>
                 </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
