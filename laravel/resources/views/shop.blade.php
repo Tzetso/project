736a3162e52a47083e28eeae2dee641cf981c516
Yana <y.m.ivanova23@gmail.com>
2016-04-12 22:47:47 +0300

    shop design added

--- laravel/resources/views/shop.blade.php
+++ laravel/resources/views/shop.blade.php
@@ -1,18 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-	
-	    <div class="row">
-	        <div class="col-md-10 col-md-offset-1">	        
-	            <div class="panel panel-default">	            
-	                <div class="panel-heading"></div>
-	                
+    <div class="table-container">
+        <div class="table">
 	                <form class="form-horizontal" role="form" method="POST" action="">    
 	                	{!! csrf_field() !!}   
 	                	{{ method_field('PATCH')}}       
 	                	
-		                <table class="table">
+		                <table>
 		                
 		                	<thead>		                	
 		                		<tr>
@@ -49,7 +44,7 @@
 				                     	<td>{{$skin->price}}</td>				                     				                     	
 				                     	@if($money >= $skin->price)
 					                     	<td>
-					                     		<button name="button" value="{{$skin->id}}" type="submit" class=".btn-default">Buy</button>
+					                     		<button name="button" value="{{$skin->id}}" type="submit">Buy</button>
 					                     	</td>
 					                    @else
 					                     	<td>Not enough cash</td>
@@ -60,9 +55,6 @@
 		                	        
 		                </table>
 		             </form>
-	 
-	            </div>
-	        </div>
-	    </div>
-	</div>
+		</div>
+    </div>
 @endsection
